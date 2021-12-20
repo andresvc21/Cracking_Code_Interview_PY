425af71670e53395dd7e6ec6d9f9584c0440b554
andresv21 <iaeroespacial@gmail.com>
2021-12-20 18:35:12 -0500

    fstring from Python course

---
+++ f_String.py
@@ -0,0 +1,6 @@
+age=10
+name='bob'
+print("my name is %s. I'm %s years old"%(name, age))
+print("my name is {0}. I'm {1} years old".format(name, age))
+print("my name is {name}. I'm {age} years old".format(name=name, age=age))
+print(f"my name is {name}. I'm {age} years old")
